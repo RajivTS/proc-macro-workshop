f7c939c715b75c2d8c7c6e69040b1f61ed2f4507
Rajiv Sharma <rajshar.email@gmail.com>
2023-03-05 15:06:26 +0000

    Debug Proc Macro: Final Stage

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -18,23 +18,37 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             .to_compile_error()
             .into();
     };
+    let custom_debug_bound = match bound_attr(&input.attrs) {
+        Ok(bounds) => bounds,
+        Err(e) => return e.to_compile_error().into(),
+    };
     let mut non_phatom_generic_idents = HashSet::new();
     let mut associated_types = HashSet::new();
+    let mut custom_debug_bounds = HashSet::new();
     for field in fields.iter() {
-        if let Some(generic_type) = non_phantom_generic_field_type(&field.ty) {
-            non_phatom_generic_idents.insert(generic_type);
-        }
-        if let Some(associated_type) = associated_type(&field.ty) {
-            associated_types.insert(associated_type);
+        let bounds = match bound_attr(&field.attrs) {
+            Ok(bounds) => bounds,
+            Err(e) => return e.to_compile_error().into(),
+        };
+        if let Some(bounds) = bounds {
+            custom_debug_bounds.insert(bounds);
+        } else {
+            if let Some(generic_type) = non_phantom_generic_field_type(&field.ty) {
+                non_phatom_generic_idents.insert(generic_type);
+            }
+            if let Some(associated_type) = associated_type(&field.ty) {
+                associated_types.insert(associated_type);
+            }
         }
     }
-    add_debug_trait_bound(&mut input.generics, &non_phatom_generic_idents);
-    let where_clause = debug_where_clause(&associated_types);
+    let where_clause = if let Some(bound) = custom_debug_bound {
+        custom_debug_bounds.insert(bound.to_string());
+        debug_where_clause(&HashSet::new(), &custom_debug_bounds)
+    } else {
+        add_debug_trait_bound(&mut input.generics, &non_phatom_generic_idents);
+        debug_where_clause(&associated_types, &custom_debug_bounds)
+    };
     let (impl_generics, ty_generics, _) = input.generics.split_for_impl();
-    // let print_fields = fields.iter().map(|f| {
-    //     eprintln!("Field: {:#?}\n\n", f.ty);
-    //     quote!(())
-    // });
     let debug_fields = fields.iter().map(|f| {
         let custom_debug = debug_attr(&f.attrs)?;
         match custom_debug {
@@ -60,9 +74,58 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 .finish()
             }
         }
-        // #(#print_fields)*
     };
     proc_macro::TokenStream::from(expanded)
+}
+
+fn bound_attr(attrs: &Vec<syn::Attribute>) -> syn::Result<Option<String>> {
+    for attr in attrs {
+        if attr.path.is_ident("debug") {
+            let meta_list = match attr.parse_meta()? {
+                syn::Meta::List(meta_list) => meta_list,
+                _ => return Ok(None),
+            };
+            if meta_list
+                .path
+                .segments
+                .first()
+                .map(|segment| segment.ident.to_string() != "debug")
+                .unwrap_or(false)
+            {
+                return Err(syn::Error::new_spanned(
+                    meta_list,
+                    "expected `#[debug(bound = \"...\")]`",
+                ));
+            }
+            for nested_meta in meta_list.nested {
+                if let syn::NestedMeta::Meta(meta) = nested_meta {
+                    let name_value = match meta {
+                        syn::Meta::NameValue(nv) => nv,
+                        _ => {
+                            return Err(syn::Error::new_spanned(
+                                meta,
+                                "expected `#[debug(bound = \"...\")]`",
+                            ))
+                        }
+                    };
+                    if !name_value.path.is_ident("bound") {
+                        return Err(syn::Error::new_spanned(
+                            name_value,
+                            "expected `#[debug(bound = \"...\")]`",
+                        ));
+                    }
+                    return match &name_value.lit {
+                        syn::Lit::Str(s) => Ok(Some(s.value())),
+                        lit => Err(syn::Error::new_spanned(
+                            lit,
+                            "expected `#[debug(bound = \"...\")]`",
+                        )),
+                    };
+                }
+            }
+        }
+    }
+    Ok(None)
 }
 
 fn debug_attr(attrs: &Vec<syn::Attribute>) -> syn::Result<Option<String>> {
@@ -71,12 +134,7 @@ fn debug_attr(attrs: &Vec<syn::Attribute>) -> syn::Result<Option<String>> {
             let meta = attr.parse_meta()?;
             let name_value = match meta {
                 syn::Meta::NameValue(nv) => nv,
-                _ => {
-                    return Err(syn::Error::new_spanned(
-                        meta,
-                        "expected `#[debug = \"...\"]`",
-                    ))
-                }
+                _ => return Ok(None),
             };
             if !name_value.path.is_ident("debug") {
                 return Err(syn::Error::new_spanned(
@@ -111,14 +169,18 @@ fn add_debug_trait_bound(
 
 fn debug_where_clause(
     associated_types: &HashSet<(proc_macro2::Ident, proc_macro2::Ident)>,
+    custom_bounds: &HashSet<String>,
 ) -> Option<syn::WhereClause> {
     let mut where_clauses = vec![];
     for (core_ty, associated_ty) in associated_types {
         where_clauses.push(format!(
-            "{}::{}: Debug",
+            "{}::{}: ::std::fmt::Debug",
             core_ty.to_string(),
             associated_ty.to_string()
         ));
+    }
+    for custom_bound in custom_bounds {
+        where_clauses.push(custom_bound.to_string());
     }
     if where_clauses.len() == 0 {
         None
